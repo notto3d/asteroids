1e02e98236ead64658911b127dd62979648ad67e
notto3d <notto3d@gmail.com>
2026-03-05 09:42:41 -0800

    B: tick rate added and set to 60

--- main.py
+++ main.py
@@ -8,6 +8,8 @@ def main():
     print("Starting Asteroids with pygame version: 2.6.1")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+    clock = pygame.time.Clock()
+    dt = 0
     while True:
         log_state()
         for event in pygame.event.get():
@@ -15,6 +17,8 @@ def main():
                 return
         screen.fill("black")
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
+        print(dt)
 
 
 if __name__ == "__main__":
