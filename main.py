65e0e5e3a426bb1fb0764f8db860a08a7d6ac36e
notto3d <notto3d@gmail.com>
2026-03-05 14:49:48 -0800

    C: added circleshape.py, player.py, updated constants.py, updated main.py

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import pygame
 from constants import SCREEN_HEIGHT, SCREEN_WIDTH
 from logger import log_state
+from player import Player
+
 
 def main():
     pygame.init()
@@ -10,15 +12,18 @@ def main():
     print(f"Screen height: {SCREEN_HEIGHT}")
     clock = pygame.time.Clock()
     dt = 0
+    x = SCREEN_WIDTH / 2
+    y = SCREEN_HEIGHT / 2
+    player = Player(x, y)
     while True:
         log_state()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
         dt = clock.tick(60) / 1000
-        print(dt)
 
 
 if __name__ == "__main__":
