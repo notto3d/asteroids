ea8c6945df57052427c784277c134be5d6db7800
notto3d <notto3d@gmail.com>
2026-03-05 17:02:48 -0800

    D: add player rotation updated constants.py, player.py, and main.py

--- main.py
+++ main.py
@@ -20,10 +20,13 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        dt = clock.tick(60) / 1000
         screen.fill("black")
+        player.update(dt)
         player.draw(screen)
         pygame.display.flip()
-        dt = clock.tick(60) / 1000
+        
+        
 
 
 if __name__ == "__main__":
